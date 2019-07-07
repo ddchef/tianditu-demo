8744e7d943bc983314679e1de2c213c46b9ee89a
Nash <nash@NashdeMBP.lan>
2019-07-08 00:11:18 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # tdt-test
 
+> **运行demo需要安装vue3.0**
 ## Project setup
 ```
 yarn install
@@ -10,20 +11,17 @@ yarn install
 yarn run serve
 ```
 
-### Compiles and minifies for production
+### 安装leaflet
+
 ```
-yarn run build
+yarn add leaflet
 ```
 
-### Run your tests
+### 添加`chinatmsproviders.js`文件
+```js
+import L from 'leaflet'
+// 载入插件
+import Provider from './chinatmsproviders'
+// 载入样式
+require('leaflet/dist/leaflet.css')
 ```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="map" style="width:1200px;height:600px">
   </div>
 </template>
 
 <script>
+import L from 'leaflet'
+// 载入插件
+import Provider from './chinatmsproviders'
+// 载入样式
+require('leaflet/dist/leaflet.css')
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Map',
+  components:{
+  },
+  data(){
+    return {
+    }
+  },
+  created(){
+    // 挂载插件
+    Provider(L)
+  },
+  mounted(){
+    let myCenter = new L.LatLng(30.236169,120.261493);
+    let map = L.map('map',{
+      center: myCenter,
+      zoom: 18
+    })
+    // 使用天地图
+    L.tileLayer.chinaProvider('TianDiTu.Normal.Map',{maxZoom:18,minZoom:5,key:'28ef75572550634abe07759e8c0da364'}).addTo(map);
+    L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion',{maxZoom:18,minZoom:5,key:'28ef75572550634abe07759e8c0da364'}).addTo(map);
+    //加点
+    var marker = new L.Marker(myCenter);
+    map.addLayer(marker)
+    marker.setIcon(L.icon({   
+      iconUrl:"https://unpkg.com/leaflet@1.0.0/dist/images/marker-icon.png",
+      className: 'anim-custom'
+    }))
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
